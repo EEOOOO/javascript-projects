996761d6474e16d9b4ecdc0d064de4c2179943ee
WooYoon-local <dndbs14@gmail.com>
2022-07-19 16:11:03 +0900

    Fix: RGB Color Game App: make unvisible the clicked box which is not the answer

---
+++ .history/rgbColorGameApp/rgbColorGame_20220719161041.js
@@ -0,0 +1,75 @@
+let playGameBtn = document.querySelector('.playNewGameButton');
+let stateMessage = document.querySelector('.stateMessage');
+let header = document.querySelector('.header');
+let rgbQuestion = document.querySelector('.rgbQuestion');
+let gameModes = document.querySelector('.modes');
+let cssRoot = document.querySelector(":root");
+let colorBox = document.querySelectorAll(".colorBox");
+
+// playGameBtn누를 때, easy/hard누를 때 게임 초기화 시행
+playGameBtn.addEventListener('click',()=>{
+    console.log('play');
+    initGame();
+});
+gameModes.addEventListener('click',(event)=>{
+    if (event.target.tagName == 'SPAN'){
+        initGame();
+    }
+})
+
+function initGame(){
+    // 랜덤 색 7개 뽑기
+    let boxNum = 7
+    let randomColors = makeRandomRGB(boxNum);
+    
+    // 그 중 하나 정답 색 , rgbQuestion 글자 넣어주기
+    let answer = randomColors[Math.floor(Math.random() * 6)];
+    rgbQuestion.textContent = answer;
+    setBoxes(randomColors, answer);
+    // 그 중 마지막 색으로 헤더 색 변경
+    let initHeaderColor = randomColors[randomColors.length-1];
+    cssRoot.style.setProperty('--color-now',initHeaderColor);
+}
+function setBoxes(randomColors, answer){
+    for (let i = 0; i < randomColors.length-1; i++){
+    colorBox[i].style.backgroundColor = randomColors[i];
+    colorBox[i].addEventListener('click', event => checkBoxIsAnswer(event, answer));
+    }
+}
+function checkBoxIsAnswer(event, answer){
+    if (answer[0] == event.target.style.backgroundColor){
+        for (box of colorBox){
+            box.style.backgroundColor = answer[0];
+            cssRoot.style.cssText = `--color-now:${answer[0]}`;
+        }
+    }
+    else{
+        event.target.style.visibility = 'hidden';
+    }
+}
+function makeRandomRGB(num){
+    let randomColors = [];
+    for (let i = 0; i < num; i++){ 
+        let r = getRandomArbitrary(1,255);
+        let g = getRandomArbitrary(1,255);
+        let b = getRandomArbitrary(1,255);
+        randomColors.push([`rgb(${r}, ${g}, ${b})`]);
+    }
+    return randomColors;
+}
+// min, max bind로 값 고정 못 시키나?
+function getRandomArbitrary(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+function winGame(){
+    // header 색 바꾸기
+    // playGameBtn 문구 바꾸기
+    // stateMessage변경 (correct)
+    // 모든 박스 색 바꾸기
+}
+function playGame(){
+    // 박스 눌리면 
+        // 정답인지 체크
+        // 맞으면 winGame()
+        // 아니면 stateMessage 변경 (try again), 그 박스 제거
+}
